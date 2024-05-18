bf33c5be48f4e8f0cd8191178bdac0c938544c3e
danmandel <dan19926@gmail.com>
2024-05-18 00:33:02 -0400

    trait code golf

--- src/datastructures/client.rs
+++ src/datastructures/client.rs
@@ -12,8 +12,5 @@ pub trait TradingClient {
         Self: Sized;
     async fn create_order(&self, order: &Order) -> Result<(), Box<dyn std::error::Error>>; // TODO: OrderResponse
     async fn get_asset(&self, symbol: &str) -> Result<Asset, Box<dyn std::error::Error>>;
-    async fn subscribe_to_data(
-        &self,
-        symbol: &str,
-    ) -> std::result::Result<(), Box<dyn std::error::Error>>;
+    async fn subscribe_to_data(&self, symbol: &str) -> Result<(), Box<dyn std::error::Error>>;
 }
