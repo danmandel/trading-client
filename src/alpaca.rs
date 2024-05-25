fcb167d2cdc49f589c4c2e8ce853e247a17b5a0c
danmandel <dan19926@gmail.com>
2024-05-24 23:05:43 -0400

    subscription wip

--- src/alpaca.rs
+++ src/alpaca.rs
@@ -1,5 +1,5 @@
 use crate::datastructures::asset::Asset;
-use crate::datastructures::client::{FeedType, TradingClient};
+use crate::datastructures::client::{FeedType, SubscriptionRequest, TradingClient};
 use crate::datastructures::config::Config;
 use crate::datastructures::order::Order;
 use async_trait::async_trait;
@@ -7,53 +7,52 @@ use futures_util::{SinkExt, StreamExt};
 use reqwest::{header::HeaderMap, Client as HttpClient};
 use serde_json::{json, to_string_pretty};
 use tokio::net::TcpStream;
-use tokio_tungstenite::{ MaybeTlsStream, WebSocketStream};
 use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
+use tokio_tungstenite::{MaybeTlsStream, WebSocketStream};
 use url::Url;
 
-pub struct SubScriptionRequest {
-    pub action: &'static str,
-    pub trades: Vec<&'static str>,
-    pub quotes: Vec<&'static str>,
-    pub bars: Vec<&'static str>,
-    pub updated_bars: Vec<&'static str>, // camelcase?
-    pub daily_bars: Vec<&'static str>, // camelcase?
-    pub orderbooks: Vec<&'static str>
-}
+fn get_ws_url(feed_type: FeedType, enable_real_trading: bool) -> String {
+    let src = "iex"; // "sip" requires subscription
 
-fn get_ws_url(feed_type: FeedType, enable_real_trading: bool) -> &'static str {
     match feed_type {
         // Docs: https://docs.alpaca.markets/docs/real-time-stock-pricing-data
         FeedType::Stocks => {
             if enable_real_trading {
-                "wss://api.alpaca.markets/stream" // "wss://stream.data.alpaca.markets/v2/iex"
+                format!("wss://stream.data.alpaca.markets/v2/{src}")
             } else {
-                //"wss://stream.data.alpaca.markets/v2/test" 
-                "wss://paper-api.alpaca.markets/stream" // "wss://paper-stream.data.alpaca.markets/v2/iex"
+                format!("wss://stream.data.sandbox.alpaca.markets/v2/{src}")
             }
         }
         // Docs: https://docs.alpaca.markets/docs/real-time-crypto-pricing-data
         FeedType::Crypto => {
             if enable_real_trading {
-                "wss://stream.data.alpaca.markets/v1beta3/crypto/us"
+                format!("wss://stream.data.alpaca.markets/v1beta3/crypto/us")
             } else {
-                "wss://stream.data.sandbox.alpaca.markets/v1beta3/crypto/us"
+                format!("wss://stream.data.sandbox.alpaca.markets/v1beta3/crypto/us")
             }
         }
         // Docs: https://docs.alpaca.markets/docs/streaming-real-time-news
         FeedType::News => {
             if enable_real_trading {
-                "wss://stream.data.alpaca.markets/v1beta1/news"
+                format!("wss://stream.data.alpaca.markets/v1beta1/news")
             } else {
-                "wss://stream.data.sandbox.alpaca.markets/v1beta1/news"
+                format!("wss://stream.data.sandbox.alpaca.markets/v1beta1/news")
             }
         }
         // Docs: https://docs.alpaca.markets/docs/real-time-option-data
         FeedType::Options => {
             if enable_real_trading {
-                "wss://stream.data.alpaca.markets/v1beta1/{feed}"
+                format!("wss://stream.data.alpaca.markets/v1beta1/{src}")
             } else {
-                "wss://stream.data.sandbox.alpaca.markets/v1beta1/{feed}" // TODO: Substitute indicative or opra to {feed} depending on your subscription.
+                format!("wss://stream.data.sandbox.alpaca.markets/v1beta1/{src}")
+                // TODO: Substitute indicative or opra to {feed} depending on your subscription.
+            }
+        }
+        FeedType::Test => {
+            if enable_real_trading {
+                format!("wss://stream.data.alpaca.markets/v2/test")
+            } else {
+                format!("wss://stream.data.alpaca.markets/v2/test")
             }
         }
     }
@@ -86,7 +85,7 @@ impl TradingClient for AlpacaClient {
             enable_real_trading: config.enable_real_trading,
         }
     }
- 
+
     // TODO: what if order was its own struct that had adjust_for_confidence and adjust_for_kelly_criteron
     async fn create_order(&self, order: &Order) -> Result<(), Box<dyn std::error::Error>> {
         let url = format!("{}/v2/orders", self.base_url);
@@ -112,67 +111,135 @@ impl TradingClient for AlpacaClient {
     // async fn close_order();
     // async fn close_all_orders();
 
-    /// Docs: https://docs.alpaca.markets/docs/streaming-market-data
     async fn subscribe(
         &self,
-        symbol: &str,
+        request: SubscriptionRequest,
         feed_type: FeedType,
     ) -> Result<WebSocketStream<MaybeTlsStream<TcpStream>>, Box<dyn std::error::Error>> {
         let ws_url = get_ws_url(feed_type, self.enable_real_trading);
-        let url = Url::parse(ws_url)?;
-
+        let url = Url::parse(&ws_url)?;
         let (mut socket, response) = connect_async(url).await?;
-
+    
         if response.status() != 101 {
             eprintln!("Failed to connect, status code: {}", response.status());
             return Err("Connection failed with non-101 status code".into());
         }
-
+    
         println!("Connected to the server: {:?}", response);
-
+        println!("key {:?}", self.api_key);
+        println!("secret {:?}", self.secret_key);
+        println!("base {:?}", self.base_url);
+    
         // Authenticate with your API key
         let auth_message = json!({
             "action": "authenticate",
             "data": {
                 "key_id": self.api_key,
-                "secret_key": self.secret_key  // Ensure these are correctly formatted
+                "secret_key": self.secret_key // Ensure these are correctly formatted
             }
         });
 
-        socket.send(Message::Text(auth_message.to_string())).await?;
 
+    
+        socket.send(Message::Text(auth_message.to_string())).await?;
+    
         // Waiting for authentication response
-        if let Some(message) = socket.next().await {
+        while let Some(message) = socket.next().await {
             match message? {
                 Message::Text(text) => {
                     println!("Authentication response: {}", text);
                     if text.contains("unauthorized") || text.contains("error") {
-                        return Err("Authentication failed".into());
+                        return Err("!!Authentication failed".into());
+                    } else if text.contains("authenticated") {
+                        break; // Exit the loop if authentication is successful
                     }
                 }
-                _ => (),
+                _ => (), // Handle other message types if necessary
             }
         }
-
-
-        // Subscribe to minutely updates for the specified symbol.
-        let subscribe_message = json!({
-            "action": "subscribe",
-            "bars": ["AAPL"],
-            "trades": ["AAPL"],
-            "quotes": ["AAPL"],
-            "daily_bars": ["AAPL"]            
-        });
-
-        let message_string = to_string_pretty(&subscribe_message)?;
+    
+        let subscription_request = json!(request);
+        let message_string = to_string_pretty(&subscription_request)?;
         println!("Serialized JSON: {}", message_string);
-
-        socket
-            .send(Message::Text(subscribe_message.to_string()))
-            .await?;
-
+    
+        // socket
+        //     .send(Message::Text(subscription_request.to_string()))
+        //     .await?;
+    
         Ok(socket)
     }
+
+    /// Docs: https://docs.alpaca.markets/docs/streaming-market-data
+    // async fn subscribe(
+    //     &self,
+    //     request: SubscriptionRequest,
+    //     feed_type: FeedType,
+    // ) -> Result<WebSocketStream<MaybeTlsStream<TcpStream>>, Box<dyn std::error::Error>> {
+    //     let ws_url = get_ws_url(feed_type, self.enable_real_trading);
+    //     let url = Url::parse(&ws_url)?;
+
+    //     let (mut socket, response) = connect_async(url).await?;
+
+    //     if response.status() != 101 {
+    //         eprintln!("Failed to connect, status code: {}", response.status());
+    //         return Err("Connection failed with non-101 status code".into());
+    //     }
+
+    //     println!("Connected to the server: {:?}", response);
+
+    //     // Authenticate with your API key
+    //     let auth_message = json!({
+    //         "action": "authenticate",
+    //         "data": {
+    //             "key_id": self.api_key,
+    //             "secret_key": self.secret_key  // Ensure these are correctly formatted
+    //         }
+    //     });
+
+    //     socket.send(Message::Text(auth_message.to_string())).await?;
+
+    //     // Waiting for authentication response
+    //     if let Some(message) = socket.next().await {
+    //         match message? {
+    //             Message::Text(text) => {
+    //                 println!("Authentication response: {}", text);
+    //                 if text.contains("unauthorized") || text.contains("error") {
+    //                     return Err("Authentication failed".into());
+    //                 }
+    //                 // Authentication successful, proceed with subscription
+    //                 let subscription_request = json!(request);
+    //                 let message_string = to_string_pretty(&subscription_request)?;
+    //                 println!("Serialized JSON: {}", message_string);
+    //                 socket
+    //                     .send(Message::Binary(
+    //                         subscription_request.to_string().into_bytes(),
+    //                     ))
+    //                     .await?;
+    //             }
+    //             _ => {
+    //                 return Err("Unexpected authentication response".into());
+    //             }
+    //         }
+    //     } else {
+    //         return Err("No authentication response received".into());
+    //     }
+
+    //     // let subscription_request = json!(request);
+
+    //     // let message_string = to_string_pretty(&subscription_request)?;
+    //     // println!("Serialized JSON: {}", message_string);
+
+    //     // socket
+    //     // // .send(Message::Text(
+    //     // //     subscription_request.to_string(),
+    //     // // ))
+    //     //     .send(Message::Binary(
+    //     //         subscription_request.to_string().into_bytes(),
+    //     //     ))
+    //     //     .await?;
+
+    //     Ok(socket)
+    // }
 
     async fn get_asset(&self, symbol: &str) -> Result<Asset, Box<dyn std::error::Error>> {
         let url = format!("{}/v2/assets/{}", self.base_url, symbol);
--- src/datastructures/client.rs
+++ src/datastructures/client.rs
@@ -4,14 +4,92 @@ use super::{
     order::{Order, OrderResponse},
 };
 use async_trait::async_trait;
+use serde::Serialize;
 use tokio::net::TcpStream;
-use tokio_tungstenite::{ MaybeTlsStream, WebSocketStream};
+use tokio_tungstenite::{MaybeTlsStream, WebSocketStream};
 
 pub enum FeedType {
     Stocks,
     Crypto,
     News,
     Options,
+    Test,
+}
+#[derive(Serialize)]
+pub struct SubscriptionRequest {
+    /// Always "subscribe"
+    pub action: &'static str,
+    /// Array of ticker symbols ex. ["AAPL"] or ["BTC"]
+    pub trades: Vec<&'static str>,
+    // pub quotes: Vec<&'static str>,
+    // pub bars: Vec<&'static str>,
+    // pub updated_bars: Vec<&'static str>, // camelcase?
+    // pub daily_bars: Vec<&'static str>,   // camelcase?
+    // pub orderbooks: Vec<&'static str>,
+}
+
+pub struct SubscriptionRequestBuilder {
+    trades: Vec<&'static str>,
+    quotes: Vec<&'static str>,
+    bars: Vec<&'static str>,
+    updated_bars: Vec<&'static str>,
+    daily_bars: Vec<&'static str>,
+    orderbooks: Vec<&'static str>,
+}
+
+impl SubscriptionRequestBuilder {
+    pub fn new() -> Self {
+        SubscriptionRequestBuilder {
+            trades: vec![],
+            quotes: vec![],
+            bars: vec![],
+            updated_bars: vec![],
+            daily_bars: vec![],
+            orderbooks: vec![],
+        }
+    }
+
+    pub fn trades(mut self, trades: &[&'static str]) -> Self {
+        self.trades = trades.to_vec();
+        self
+    }
+
+    pub fn quotes(mut self, quotes: &[&'static str]) -> Self {
+        self.quotes = quotes.to_vec();
+        self
+    }
+
+    pub fn bars(mut self, bars: &[&'static str]) -> Self {
+        self.bars = bars.to_vec();
+        self
+    }
+
+    pub fn updated_bars(mut self, updated_bars: &[&'static str]) -> Self {
+        self.updated_bars = updated_bars.to_vec();
+        self
+    }
+
+    pub fn daily_bars(mut self, daily_bars: &[&'static str]) -> Self {
+        self.daily_bars = daily_bars.to_vec();
+        self
+    }
+
+    pub fn orderbooks(mut self, orderbooks: &[&'static str]) -> Self {
+        self.orderbooks = orderbooks.to_vec();
+        self
+    }
+
+    pub fn build(self) -> SubscriptionRequest {
+        SubscriptionRequest {
+            action: "subscribe",
+            trades: self.trades,
+            // quotes: self.quotes,
+            // bars: self.bars,
+            // updated_bars: self.updated_bars,
+            // daily_bars: self.daily_bars,
+            // orderbooks: self.orderbooks,
+        }
+    }
 }
 
 #[async_trait]
@@ -23,7 +101,7 @@ pub trait TradingClient {
     async fn get_asset(&self, symbol: &str) -> Result<Asset, Box<dyn std::error::Error>>;
     async fn subscribe(
         &self,
-        symbol: &str,
+        request: SubscriptionRequest,
         feed_type: FeedType,
     ) -> Result<WebSocketStream<MaybeTlsStream<TcpStream>>, Box<dyn std::error::Error>>;
 }
